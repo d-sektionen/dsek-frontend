b1c208ca64f12d26602bdbdd5a6c6ecd95d6bfd5
Isak Horvath <isakhorvath@gmail.com>
2022-10-06 18:50:21 +0200

    NEW: Add TODO for nav dropdown

--- dsek-frontend/src/components/Nav.js
+++ dsek-frontend/src/components/Nav.js
@@ -5,8 +5,46 @@ function Nav() {
     return (
         <nav className="Menu">
             <NavButton title="Sektionen" link="https://google.com"/>
+            {
+                /** TODO Sektionen dropdown
+                 *  Sektionsinfo
+                 *  Styrelsen, kansliet & SnOrdfar
+                 *  Miljö & Arbetsmiljö
+                 *  Grafisk profil
+                 *  Aktuella dokument
+                 *  Protokoll styrelsemöten
+                 */
+            }
             <NavButton title="Utskott" link="https://google.com"/>
+            {
+                /** TODO Utskott dropdown
+                 *  Eventutskottet
+                 *  Alumni
+                 *  D-Group
+                 *  DONNA - D-sektionens Damförening
+                 *  DEG
+                 *  Infoutskottet
+                 *  LINK-dagarna
+                 *  Marknadsföringsutskottet
+                 *  Näringslivsutskottet
+                 *  Pubutskottet
+                 *  STABEN
+                 *  Utbildningsutskottet
+                 *  Valberedningen
+                 *  Webbutskottet
+                 *  Werkmästeriet
+                 */
+            }
             <NavButton title="Sökande" link="https://google.com"/>
+            {
+                /** TODO Sökande dropdown
+                 *  Utbildningar
+                 *  Bostad
+                 *  Skolan
+                 *  Studentliv
+                 *  FAQ
+                 */
+            }
             <NavButton title="Alumni-blogg" link="https://google.com"/>
             <NavButton title="Exjobb" link="https://google.com"/>
             <NavButton title="Företag" link="https://google.com"/>
