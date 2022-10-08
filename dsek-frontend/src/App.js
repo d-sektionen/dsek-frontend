2758702421eb97efd2b43d671048d65dc54ac5ff
Isak Horvath <isakhorvath@gmail.com>
2022-10-08 20:07:52 +0200

    NEW: Add base for several committee pages

--- dsek-frontend/src/App.js
+++ dsek-frontend/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Layout from './components/Layout';
 import DEG from './templates/committee/DEG';
+import Valberedningen from './templates/committee/Valberedningen';
+import Alumni from './templates/committee/Valberedningen';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -11,12 +13,13 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}></Route>
         <Route path="/deg" element={<DEG />}></Route>
+        <Route path="/valberedningen" element={<Valberedningen />}></Route>
+        <Route path="/alumni" element={<Alumni />}></Route>
         {/*
-          <Route path="/alumni" element={<Alumni />}></Route>
           <Route path="/eventu" element={<EventU />}></Route>
+          <Route path="/pubu" element={<PubU />}></Route>
           <Route path="/mafu" element={<MafU />}></Route>
           <Route path="/naru" element={<NarU />}></Route>
-          <Route path="/valberedningen" element={<Valberedningen />}></Route>
           <Route path="/werk" element={<Werk />}></Route>
         */}
       </Routes>
--- dsek-frontend/src/components/Nav.js
+++ dsek-frontend/src/components/Nav.js
@@ -6,7 +6,7 @@ function Nav() {
     return (
         <nav className="Menu">
             <DropdownNavButton title="Sektionen" link="#" titles={[["Styrelsen", "#"], ["Miljö", "#"], ["Dokument", "#"], ["Protokoll", "#"]]}/>
-            <DropdownNavButton title="Utskott" link="#" titles={[["EventU", "/eventu"], ["Alumni", "/alumni"], ["D-Group", "https://d-group.se/"], ["DONNA", "https://donna.d-sektionen.se/"], ["DEG", "/deg"], ["LINK-dagarna", "https://linkdagarna.se/"], ["MafU", "/mafu"], ["NärU", "/naru"], ["STABEN", "https://staben.info/"], ["UtbU", "/utbu"], ["Valberedningen", "/valberedningen"], ["WebbU", "https://www.webbu.se/"], ["Werkmästeriet", "werk"]]}/>
+            <DropdownNavButton title="Utskott" link="#" titles={[["EventU", "/eventu"], ["Alumni", "/alumni"], ["D-Group", "https://d-group.se/"], ["DONNA", "https://donna.d-sektionen.se/"], ["DEG", "/deg"], ["LINK-dagarna", "https://linkdagarna.se/"], ["PubU", "/pubu"], ["MafU", "/mafu"], ["NärU", "/naru"], ["STABEN", "https://staben.info/"], ["UtbU", "/utbu"], ["Valberedningen", "/valberedningen"], ["WebbU", "https://www.webbu.se/"], ["Werkmästeriet", "werk"]]}/>
             <DropdownNavButton title="Sökande" link="#" titles={[["Utbildningar", "#"], ["Bostad", "#"], ["Skolan", "#"], ["Studentliv", "#"], ["FAQ", "#"]]}/>
             <NavButton title="Alumni-blogg" link="https://google.com"/>
             <NavButton title="Exjobb" link="https://google.com"/>
