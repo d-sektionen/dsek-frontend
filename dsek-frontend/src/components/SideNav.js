7a9f0481b574d1688ab1dce1b17574dd3b3c1ba6
theaborg <thebo717@student.liu.se>
2022-11-10 21:06:05 +0100

    Add functions to the css file, which color the nth child of the SideNav a specific color. Also delete unnecessary code och utkommenterad kod B)

--- dsek-frontend/src/components/SideNav.js
+++ dsek-frontend/src/components/SideNav.js
@@ -5,29 +5,13 @@ import SideNavButton from './SideNavButton.js';
 function SideNav() {
     return (
         <nav className="SideNav">
-            <SideNavButton title="Sociala medier" color="#754022"/>
-            <SideNavButton title="Kalender" color="#E5398D"/>
-            <SideNavButton title="Medlemstjänster" color="#F7E623"/>
-            <SideNavButton title="Fotoalbum" color="#70BD44"/>
-            <SideNavButton title="Kontakt" color="#20407C"/>
-        </nav>
-        /*<nav className="SideNav">
             <SideNavButton title="Sociala medier"/>
             <SideNavButton title="Kalender"/>
             <SideNavButton title="Medlemstjänster"/>
             <SideNavButton title="Fotoalbum"/>
             <SideNavButton title="Kontakt"/>
-    </nav>*/
+        </nav>
     );
 }
-
-/*
-colors:
-sociala color="#754022"
-kalender color="#E5398D"
-medlem color="#F7E623"
-foto color="#70BD44"
-kontakt color="#20407C"
-*/
 
 export default SideNav;
