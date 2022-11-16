115c72359b0b2d332d98e24612f81a2d7f4a58ed
Therese Selberg <these002@student.liu.se>
2022-11-16 17:40:09 +0100

    removed empty space below footer. fixed spacing between paragraphs. social media buttons change colour on hover.

--- dsek-frontend/src/components/Footer.js
+++ dsek-frontend/src/components/Footer.js
@@ -16,8 +16,8 @@ function Footer() {
                 <div className="Footer-Info">
                     <p><b>Postadress:</b> Datateknologsektionen, Kårallen, Universitetet, 581 83 Linköping</p>
                     <p><b>Organisationsnr:</b> 822002-1409 <b>Bankgironr:</b> 5644-8251</p>
-                    <p><b>E-post:</b> <a href="webmaster@d-sektionen.se"> Webmaster </a> </p>
-                    <p><b><a className="Icons" href="https://www.facebook.com/datateknologsektionen"> <FaFacebook/> </a></b>
+                    <p><b>E-post:</b> <a href="mailto:webmaster@d-sektionen.se"> webmaster@d-sektionen.se </a> </p>
+                    <p className='Icons'><b><a className="Icons" href="https://www.facebook.com/datateknologsektionen"> <FaFacebook/> </a></b>
                        <b><a className="Icons" href="https://www.linkedin.com/company/datateknologsektionen"> <FaLinkedin/> </a></b> 
                        <b><a className="Icons" href="https://github.com/d-sektionen"> <FaGithub/> </a></b> 
                        <b><a className="Icons" href="https://www.reddit.com/r/linkopinguniversity"> <FaReddit/> </a></b> 
