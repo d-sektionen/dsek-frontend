95834eb0fe79123d8fc8f7e6d4b47f61f87b5f47
matss_ql6qq <gabma609@student.liu.se>
2023-10-03 17:15:39 +0200

    some changes

--- src/components/DropdownNavButton.js
+++ src/components/DropdownNavButton.js
@@ -4,10 +4,10 @@ import '../css/Nav.css';
 function DropdownNavButton(props) {
     return (
       <div className="dropdown-nav-button">
+        <a href={props.link} onClick={props.onClick}>
         {props.icon && (
           <img src={props.icon} alt={`Icon for ${props.title}`} className="dropdown-icon" />
         )}
-        <a href={props.link} onClick={props.onClick}>
           {props.title}
         </a>
       </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,32 +4,64 @@ import DropdownElements from './DropdownElements.js';
 import NavButton from './NavButton.js';
 
 function Nav({ setState }) {
+
+    const dropdownItemsSektionen = [
+        ["Styrelsen", "/page/styrelsen"],
+        ["Miljö", "/page/miljo"],
+        ["Dokument", "/page/dokument"],
+        ["Protokoll", "/page/protokoll"]
+      ];
+      
+    const dropdownItemsUtskott = [
+      ["EventU", "/eventu"],
+      ["Alumni", "/alumni", "https://d-sektionen.se/wp-content/uploads/2019/09/Webp.net-resizeimage.png"],
+      ["D-Group", "https://d-group.se/", "https://d-sektionen.se/wp-content/uploads/2015/04/d-group.png"],
+      ["DONNA", "https://donna.d-sektionen.se/", "https://donna.d-sektionen.se/wp-content/uploads/2019/08/cropped-output-onlinepngtools-1-100x49.png"],
+      ["DEG", "/deg"],
+      ["LINK-dagarna", "https://linkdagarna.se/", "https://d-sektionen.se/wp-content/uploads/2020/11/logotyp_linkdagarna.png"],
+      ["PubU", "/pubu", "https://d-sektionen.se/wp-content/uploads/2018/09/pubu-768x442.png"],
+      ["MafU", "/mafu"],
+      ["NärU", "/naru", "https://d-sektionen.se/wp-content/uploads/2018/09/Logga-NarU-vit-768x568.png"],
+      ["STABEN", "https://staben.info/", "https://staben.info/images/logo.png"],
+      ["UtbU", "/utbu"],
+      ["Valberedningen", "/valberedningen"],
+      ["WebbU", "https://www.webbu.se/", "https://d-sektionen.se/wp-content/uploads/2020/10/webbgruppen-logo-1-2048x803.png"],
+      ["Werkmästeriet", "/werk"]
+    ];
+      
+    const dropdownItemsBliStudent = [
+      ["Utbildningar", "/page/utbildningar"],
+      ["Bostad", "/page/bostad"],
+      ["Skolan", "/page/skolan"],
+      ["Studentliv", "/page/studentliv"],
+      ["FAQ", "/page/faq"]
+    ];
+      
+    const dropdownItemsExtra = [
+      ["Sociala medier", "/socialamedier"],
+      ["Alumni-blogg", "https://d-sektionen.se/kategori/alumni-blogg/"],
+      ["Exjobb", "https://d-sektionen.se/kategori/exjobb/"],
+      ["Fotoalbum", "https://d-sektionen.se/filarkiv/fotoalbum/"]
+    ];
+      
     return (
-        <nav className="site-nav">
-            <Dropdown content="Sektionen">
-                <DropdownElements setState={setState} titles={[["Styrelsen", "/page/styrelsen"], ["Miljö", "/page/miljo"],
-                ["Dokument", "/page/dokument"], ["Protokoll", "/page/protokoll"]]} />
-            </Dropdown>
-            <Dropdown content="Utskott">
-                <DropdownElements setState={setState} titles={[["EventU", "/eventu"], ["Alumni", "/alumni"],
-                ["D-Group", "https://d-group.se/", "https://d-sektionen.se/wp-content/uploads/2015/04/d-group.png"], 
-                ["DONNA", "https://donna.d-sektionen.se/", "https://donna.d-sektionen.se/wp-content/uploads/2019/08/cropped-output-onlinepngtools-1-100x49.png"], 
-                ["DEG", "/deg"], ["LINK-dagarna", "https://linkdagarna.se/"], ["PubU", "/pubu", "https://d-sektionen.se/wp-content/uploads/2018/09/pubu-768x442.png"], ["MafU", "/mafu"],
-                ["NärU", "/naru"], ["STABEN", "https://staben.info/", "https://staben.info/images/logo.png"], ["UtbU", "/utbu"], ["Valberedningen", "/valberedningen"],
-                ["WebbU", "https://www.webbu.se/"], ["Werkmästeriet", "/werk"]]} />
-            </Dropdown>
-            <Dropdown content="Bli student">
-                <DropdownElements setState={setState} titles={[["Utbildningar", "/page/utbildningar"], ["Bostad", "/page/bostad"],
-                ["Skolan", "/page/skolan"], ["Studentliv", "/page/studentliv"], ["FAQ", "/page/faq"]]} />
-            </Dropdown>
-            <NavButton title="Kalender" link="/kalender" />
-            <NavButton title="Medlemstjänster" link="https://medlem.d-sektionen.se" />
-            <NavButton title="Företag" link="/page/foretag" />
-            <Dropdown content="Extra">
-                <DropdownElements setState={setState} titles={[["Sociala medier", "/socialamedier"], ["Alumni-blogg", "https://d-sektionen.se/kategori/alumni-blogg/"], 
-                ["Exjobb", "https://d-sektionen.se/kategori/exjobb/"], ["Fotoalbum", "https://d-sektionen.se/filarkiv/fotoalbum/"]]} />
-            </Dropdown>
-        </nav >
+      <nav className="site-nav">
+        <Dropdown content="Sektionen">
+          <DropdownElements setState={setState} titles={dropdownItemsSektionen} />
+        </Dropdown>
+        <Dropdown content="Utskott">
+          <DropdownElements setState={setState} titles={dropdownItemsUtskott} />
+        </Dropdown>
+        <Dropdown content="Bli student">
+          <DropdownElements setState={setState} titles={dropdownItemsBliStudent} />
+        </Dropdown>
+        <NavButton title="Kalender" link="/kalender" />
+        <NavButton title="Medlemstjänster" link="https://medlem.d-sektionen.se" />
+        <NavButton title="Företag" link="/page/foretag" />
+        <Dropdown content="Extra">
+          <DropdownElements setState={setState} titles={dropdownItemsExtra} />
+        </Dropdown>
+      </nav>
     );
 }
 
