b9eeca9a97db2a70254c58adf864af07214e7924
Mafrans <malte.kluft@gmail.com>
2025-03-10 19:21:34 +0100

    feat: finish post list styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { apiFetch } from "@/util/api";
 import { Post } from "@/util/strapi";
 import { useSearchParams } from "next/navigation";
 import dayjs from "dayjs";
+import Link from "next/link";
 
 const PAGE_SIZE = 10;
 export default async function HomePage({
@@ -23,11 +24,17 @@ export default async function HomePage({
   return (
     <div>
       <ul>
-        {posts?.map(({ attributes: { title, publishedAt, excerpt } }) => (
-          <li>
-            <h3>
-              {title} - {dayjs(publishedAt).format("ddd ll [kl.]LT")}
-            </h3>
+        {posts?.map(({ attributes: { title, slug, publishedAt, excerpt } }) => (
+          <li className={styles.post}>
+            <Link className={styles.postTitle} href={`/post/${slug}`}>
+              <h2>{title}</h2>
+            </Link>
+            <hr />
+            <p>
+              <time dateTime={dayjs(publishedAt).format("YYYY-MM-DD")}>
+                {dayjs(publishedAt).format("YYYY-MM-DD")}
+              </time>
+            </p>
             <p>{excerpt}</p>
           </li>
         ))}
