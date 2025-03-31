4207143cdf1e1e857409ebb9ceea6637d1f1c4a8
Mafrans <malte.kluft@gmail.com>
2025-03-31 18:58:15 +0200

    feat: add utskott page

--- src/app/utskott/[slug]/page.tsx
+++ src/app/utskott/[slug]/page.tsx
@@ -1,3 +1,38 @@
-export default function UtskottPage() {
-  return <></>;
+import { apiFetch, apiFetchOne } from "@/util/api";
+import { Utskott } from "@/util/strapi";
+import { notFound } from "next/navigation";
+
+export async function getStaticPaths() {
+  const { data: utskotts = [] } = await apiFetch<Utskott[]>("/utskotts");
+
+  return {
+    paths: utskotts.map(({ attributes }) => ({
+      params: { slug: attributes.slug },
+    })),
+    fallback: false,
+  };
+}
+
+export default async function UtskottPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+  const { data: utskott } = await apiFetchOne<Utskott>(`/utskotts`, {
+    filters: { slug },
+  });
+
+  if (utskott == null) {
+    return notFound();
+  }
+
+  const { title, summary } = utskott.attributes;
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{summary}</p>
+    </div>
+  );
 }
--- src/app/utskott/page.tsx
+++ src/app/utskott/page.tsx
@@ -1,17 +1,23 @@
 import { apiFetch } from "@/util/api";
 import { Utskott } from "@/util/strapi";
+import style from "./page.module.css";
+import Link from "next/link";
 
 export default async function UtskottPage() {
   const { data: utskott } = await apiFetch<Utskott[]>("/utskotts");
 
   return (
     <div>
-      <h1>Utskott</h1>
+      <div className={style.header}>
+        <h1>Utskott på D-Sektionen</h1>
+        <hr />
+      </div>
       <ul>
         {utskott?.map(({ id, attributes }) => (
-          <li key={id}>
+          <li className={style.utskott} key={id}>
             <h2>{attributes.title}</h2>
             <p>{attributes.summary}</p>
+            <Link href={`/utskott/${attributes.slug}`}>Läs mer</Link>
           </li>
         ))}
       </ul>
