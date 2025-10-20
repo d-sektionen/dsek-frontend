33a230e81dbf21962140fe85a82485c30ad18be6
Mafrans <malte.kluft@gmail.com>
2025-10-20 18:39:53 +0200

    fix: remove unnecessary api call in Calendar

--- frontend/src/app/kalender/page.tsx
+++ frontend/src/app/kalender/page.tsx
@@ -3,11 +3,6 @@ import { calendarGetEventsBetween } from "../../util/google";
 import dayjs from "dayjs";
 
 export default async function KalenderPage() {
-  const events = await calendarGetEventsBetween(
-    dayjs().subtract(1, "year"),
-    dayjs().add(1, "year"),
-  );
-
   return (
     <div>
       <Calendar />
